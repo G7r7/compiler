3d0c1087431f9184f4190c6fc0928b9f7fbdb252
Germain RULLIER <grullier@free.fr>
2021-09-01 18:21:45 +0200

    update

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <string>
 #include "token.h"
 
 FILE *fp;
@@ -16,6 +16,79 @@ void advance(void) {
 
     switch (character) {
             case '+': next.type = tok_plus;
+                break;
+            case '-': next.type = tok_minus;
+                break;
+            case '*': next.type = tok_times;
+                break;
+            case '/': next.type = tok_divide;
+                break;
+            case '%': next.type = tok_modulo;
+                break;
+            case '!':  {
+                if((character = fgetc(fp)) == '=' ){
+                    next.type = tok_not_equals_to;
+                }else{
+                    ungetc(character,fp);
+                    next.type = tok_bang;
+                }
+            }
+                break;
+            case '&': {
+                if((character = fgetc(fp)) == '&' ){
+                    next.type = tok_and;
+                }else{
+                    ungetc(character,fp);
+                    next.type = tok_address;
+                }
+            }
+                break;
+            case '=': {
+                if((character = fgetc(fp)) == '=' ){
+                    next.type = tok_equals_to;
+                }else{
+                    ungetc(character,fp);
+                    next.type = tok_equals;
+                }
+            }
+                break;
+            case '<': {
+                if((character = fgetc(fp)) == '=' ){
+                    next.type = tok_less_or_equal;
+                }else{
+                    ungetc(character,fp);
+                    next.type = tok_less_than;
+                }
+            }
+                break;
+            case '>':  {
+                if((character = fgetc(fp)) == '=' ){
+                    next.type = tok_more_or_equal;
+                }else{
+                    ungetc(character,fp);
+                    next.type = tok_more_than;
+                }
+            }
+                break;
+            case '(': next.type = tok_left_parenthesis;
+                break;
+            case ')': next.type = tok_right_parenthesis;
+                break;
+            case '[': next.type = tok_left_bracket;
+                break;
+            case ']': next.type = tok_right_bracket;
+                break;
+            case '{': next.type = tok_left_curly;
+                break;
+            case '}': next.type = tok_right_curly;
+                break;
+            case ',': next.type = tok_comma;
+                break;
+            case ';': next.type = tok_semi_colon;
+                break;
+            default:{
+                
+            }
     }
 
 }
@@ -27,6 +100,10 @@ bool check(int type) {
     return true;
 }
 
+void erreur(char* msg) {
+    printf("%s\n", msg);
+}
+
 void accept(int type) {
     if(!check(type))
         erreur("Erreur\n");
@@ -36,17 +113,24 @@ bool eof() {
     return next.type == tok_eof;
 }
 
-void erreur(char* msg) {
-    printf("%s\n", msg);
-}
 
 int main(int argc, char const *argv[])
 {
+    current.type = 666;
+
     fp = fopen(argv[1], "r");
     if(fp == NULL) {
         perror("Can't open file");
     }
     advance();
+    printf("%d", current.type);
+
+    while (character = fgetc(fp) != EOF)
+    {
+        ungetc(character, fp);
+        advance();
+        printf("%d", current.type);
+    }
 
     return 0;
 }
