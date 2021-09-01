ed1eb194cb72ab0e8e179536a3d9d44b8b9ff03d
Germain RULLIER <grullier@free.fr>
2021-09-01 16:56:44 +0200

    Parsing char by char

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 int main(int argc, char const *argv[])
 {
-    /* code */
+    int character;
+    FILE *fp;
+
+    fp = fopen(argv[1], "r");
+    if(fp == NULL) {
+        perror("Can't open file");
+    }
+
+    while ( (character = fgetc(fp)) != EOF )  
+    {
+        printf("%c\n", character);
+    }
+
     return 0;
 }
