27c5116af74047a01d30a0225ce15063635e9a7f
Germain RULLIER <grullier@free.fr>
2021-10-30 18:27:25 +0200

    Avancement tester

--- test/tester.cpp
+++ test/tester.cpp
@@ -9,7 +9,12 @@
 #include "usage.hpp"
 #include <algorithm>
 #include <filesystem>
-#include "exec.hpp"
+#include <omp.h>
+#include <chrono>
+#include <thread>
+#include <pthread.h>
+#include <signal.h>
+#include "test.hpp"
 
 int main(int argc, char const *argv[])
 {
@@ -54,15 +59,87 @@ int main(int argc, char const *argv[])
 
         outputFile.open("log.txt", std::ios_base::app);
 
+        std::vector<std::string> filePaths;
         for (const auto& dirEntry : std::filesystem::recursive_directory_iterator(testsPath)) {
             if(dirEntry.path().extension().compare(".cmm") == 0) {
-                std::cout << dirEntry.path() << std::endl;
-                std::string compileCommand = compilerPath + " " + dirEntry.path().string() + " " + "output";
-                exec(compileCommand.c_str());
-                std::string testCommand = machinePath + " " + "output";
-                outputFile << dirEntry.path() << " : " << exec(testCommand.c_str()) << std::endl; 
+                filePaths.push_back(dirEntry.path().string());
             }
         }
+
+        pthread_t pt[filePaths.size()];
+        for (int i = 0; i < filePaths.size(); i++) {
+            int finished = 0;
+            printf("Before...\n");
+            TestArgs args = { 
+                &compilerPath,
+                &filePaths[i],
+                &machinePath,
+                &outputFile,
+                &finished
+            };
+            pthread_create(&pt[i], NULL, testProgram, (void*)&args);
+            printf("I'm the timer...\n");
+            for (size_t i = 0; i < 10; i++)
+            {
+                std::this_thread::sleep_for(std::chrono::milliseconds(100));                        
+                if(finished) {
+                    printf("Work is done!\n");
+                    break;
+                }
+                else
+                    printf("I'm still waiting ...\n");
+            }
+            if(!finished) {
+                printf("I'm still waiting and it's time out...\n");
+                pthread_kill(pt[i], 10);
+            }
+        }
+
+        // for (int i = 0; i < filePaths.size(); i++) {
+        //     double start = 0; 
+        //     double end = 0;
+        //     #pragma omp parallel
+        //     {
+        //         #pragma omp sections nowait
+        //         {
+        //             #pragma omp section
+        //             {
+        //                 #pragma omp atomic write
+        //                 pt = pthread_self();
+        //                 #pragma omp atomic write
+        //                 start = omp_get_wtime(); 
+        //                 std::cout << filePaths[i] << std::endl;
+        //                 std::string compileCommand = compilerPath + " " + filePaths[i] + " " + filePaths[i] + ".out";
+        //                 exec(compileCommand.c_str());
+        //                 std::string testCommand = machinePath + " " + filePaths[i] + ".out";
+        //                 std::string commandResult = exec(testCommand.c_str());
+        //                 #pragma omp critical
+        //                 outputFile << filePaths[i] << " : " << commandResult << std::endl; 
+        //                 #pragma omp atomic write
+        //                 end = omp_get_wtime(); 
+        //             }
+        //             #pragma omp section
+        //             {
+        //                 printf("I'm the timer...\n");
+        //                 for (size_t i = 0; i < 10; i++)
+        //                 {
+        //                     std::this_thread::sleep_for(std::chrono::milliseconds(100));                        
+        //                     if(end != 0) {
+        //                         printf("Work is done!\n");
+        //                         break;
+        //                     }
+        //                     else
+        //                         printf("I'm still waiting ...\n");
+        //                 }
+        //                 if(end == 0) {
+        //                     printf("I'm still waiting and it's time out...\n");
+        //                     pthread_cancel(pt);
+        //                 }
+        //             }
+        //         }
+        //     }
+        //     printf("Work took %f seconds\n", end - start);
+        // }
     }
     catch(std::string msg)
     {
