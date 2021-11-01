a19fc7857819f4a99ff03f69af30288cc4bb20d7
Germain RULLIER <grullier@free.fr>
2021-11-01 12:07:25 +0100

    test results

--- test/omp.cpp
+++ test/omp.cpp
@@ -6,6 +6,7 @@
 #include <chrono> 
 
 void testProgramsOmp(std::vector<std::string>* filePaths, std::string* compilerPath, std::string* machinePath, std::ofstream* outputFile) {
+    int successNb = 0;
     for (int i = 0; i < filePaths->size(); i++) {
         double start = 0; 
         double end = 0;
@@ -25,27 +26,30 @@ void testProgramsOmp(std::vector<std::string>* filePaths, std::string* compilerP
                 {
                     start = omp_get_wtime();
                     testProgram(&args);
-                    end = omp_get_wtime(); 
+                    end = omp_get_wtime();
+                    if (finished == 1)
+                        successNb++;
                 }
-                #pragma omp section
-                {
+                #pragma omp section // Timer to see if test is stuck
+                { 
                     for (size_t i = 0; i < 10; i++)
                     {
                         std::this_thread::sleep_for(std::chrono::milliseconds(100));                        
-                        if(end != 0) {
-                            printf("Work took %f seconds\n", end - start);
+                        if(finished != 0) {
+                            // printf("Work took %f seconds\n", end - start);
                             break;
                         }
                         else
                             printf("Waiting ...\n");
                     }
-                    if(end == 0) {
+                    if(finished == 0) {
                         outputFile->close();
                         printf("Timed out : please kill remaining msm instance manually...\n");
                     }
                 }
-            
             }
         }
     }
+    std::cout << "Results : " << successNb << "/" << filePaths->size() << std::endl;
+    *outputFile << "Results : " << successNb << "/" << filePaths->size() << std::endl;
 }
